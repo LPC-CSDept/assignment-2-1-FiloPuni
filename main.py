d86350ae01139fd225daca0db2e0383a4f62585c
FiloPuni <ftpuni@zonemail.clpccd.edu>
2023-06-24 00:48:41 -0700

    Code for this assignment

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
+    print("Welcome!");
+    
+    male_students = int(input("Insert Number Of Male Students Here: "));
+    female_students = int(input("Insert Number Of Female Students Here: "));
+    student_total = male_students + female_students;
+
+    m_perc = (male_students / student_total) * 100;
+    f_perc = (female_students / student_total) * 100;
+
+    print(f"The total number of students: \t {student_total}");
+    print(f"The number of males and females  \t{male_students} \t {female_students}");
+    print(f"The percentage of males and females  \t{m_perc: .2f}% \t {f_perc: .2f}%");
+
+    print("Please see RESULTS above. Thank you!");
 
     """
     ########################################
